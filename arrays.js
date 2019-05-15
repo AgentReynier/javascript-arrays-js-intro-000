1fa2152c2435c26758b5298f0da45b35ff0bd596
Corey Chriss <excelsior89@gmail.com>
2019-05-15 00:15:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,3 @@
 /* Doin' work on them arrays! */
 
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
-
-var addElementToBeginningOfArray = ([1], 'foo')
-addElementToBeginningOfArray =
